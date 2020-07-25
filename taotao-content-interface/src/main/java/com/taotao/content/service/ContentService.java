37cc9da3f039981fb56d396d851a3236f11188d4
李柏霖 <781748856@qq.com>
2020-07-26 00:59:17 +0800

    第七天 实现redis商品缓存

--- taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
+++ taotao-content-interface/src/main/java/com/taotao/content/service/ContentService.java
@@ -4,12 +4,14 @@ import com.taotao.common.pojo.EasyUIDataGridResult;
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.pojo.TbContent;
 
+import java.util.List;
+
 
 public interface ContentService {
 
 	TaotaoResult addContent(TbContent content);
 	EasyUIDataGridResult getContentList(long categoryId, Integer page, Integer rows);
-	TaotaoResult contentSave(TbContent tbContent);
 	TaotaoResult contentDelete(String[] id);
 	TaotaoResult restContentEdit(TbContent tbContent);
+	List<TbContent> getContentByid(Long cid);
 }
--- taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -7,8 +7,12 @@ import java.util.List;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.taotao.common.pojo.EasyUIDataGridResult;
+import com.taotao.common.utils.JsonUtils;
+import com.taotao.jedis.JedisClient;
 import com.taotao.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
@@ -21,7 +25,11 @@ public class ContentServiceImpl implements ContentService {
 
 	@Autowired
 	private TbContentMapper contentMapper;
-	
+	@Autowired
+	private JedisClient jedisClient;
+	@Value("${INDEX_CONTENT}")
+	private String INDEX_CONTENT;
+
 	@Override
 	public TaotaoResult addContent(TbContent content) {
 		//补全pojo的属性
@@ -29,6 +37,9 @@ public class ContentServiceImpl implements ContentService {
 		content.setUpdated(new Date());
 		//插入到内容表
 		contentMapper.insert(content);
+		//同步缓存
+		//删除对应的缓存信息
+		jedisClient.hdel("INDEX_CONTENT",content.getCategoryId().toString());
 		return TaotaoResult.ok();
 	}
 	@Override
@@ -50,23 +61,24 @@ public class ContentServiceImpl implements ContentService {
 		return result;
 	}
 
-	@Override
-	public TaotaoResult contentSave(TbContent tbContent) {
-		//补全对象的属性
-		tbContent.setCreated(new Date());
-		tbContent.setUpdated(new Date());
-		//插入到数据库
-		contentMapper.insert(tbContent);
-		return TaotaoResult.ok(tbContent);
-	}
+
 
 	@Override
 	public TaotaoResult contentDelete(String[] ids) {
+		List<TbContent> contentList=new ArrayList<>();
 		//设置条件
 		for (String id : ids) {
+			//获取当前要删的数据
+			TbContent content = contentMapper.selectByPrimaryKey(Long.parseLong(id));
+			//删除当前id数据
 			contentMapper.deleteByPrimaryKey(Long.parseLong(id));
+			contentList.add(content);
 		}
-		//删除当前id数据
+		//同步缓存
+		//删除对应的缓存信息
+		for (TbContent content:contentList) {
+			jedisClient.hdel("INDEX_CONTENT", content.getCategoryId().toString());
+		}
 		return TaotaoResult.ok();
 	}
 
@@ -78,6 +90,38 @@ public class ContentServiceImpl implements ContentService {
 		//插入到数据库
 		contentMapper.updateByPrimaryKeySelective(tbContent);
 		return TaotaoResult.ok(tbContent);
+	}
+
+	@Override
+	public List<TbContent> getContentByid(Long cid) {
+		//先查询缓存
+		//添加缓存不能影响正常业务逻辑
+		try {
+			//查询缓存
+			String json = jedisClient.hget(INDEX_CONTENT, cid + "");
+			//查询到结果，把json转换成List返回
+			if (StringUtils.isNotBlank(json)) {
+				List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+				return list;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//缓存中没有命中，需要查询数据库
+		TbContentExample contentExample = new TbContentExample();
+		TbContentExample.Criteria criteria = contentExample.createCriteria();
+		//设置查询条件
+		criteria.andCategoryIdEqualTo(cid);
+		//执行查询
+		List<TbContent> list =contentMapper.selectByExample(contentExample);
+		//把结果添加到缓存
+		try {
+			jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(list));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//返回结果
+		return list;
 	}
 
 
